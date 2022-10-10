1fa34c08f7c543aecb9561cf26ca96c38a8dc144
bmgjdm <lkn301415@163.com>
2022-10-10 16:05:34 +0800

    lkn:TEST测试类提交

--- src/test/java/com/sd/sdhr/SdHrApplicationTests.java
+++ src/test/java/com/sd/sdhr/SdHrApplicationTests.java
@@ -1,10 +1,10 @@
 package com.sd.sdhr;
 
-import com.sd.sdhr.pojo.Dog;
-import com.sd.sdhr.pojo.Person;
+
+import com.sd.sdhr.pojo.Tsdhr01;
+import com.sd.sdhr.service.impl.Tsdhr01ServiceImpl;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.boot.test.context.SpringBootTest;
 
 //单元测试
@@ -13,8 +13,14 @@ class SdHrApplicationTests {
     //@Autowired
     //Dog dog;
    //private Person person;
+
+    @Autowired
+    Tsdhr01ServiceImpl tsdhr01ServiceImpl;
+
     @Test
     void contextLoads() {
+        Tsdhr01 tsdhr01=tsdhr01ServiceImpl.queryTsdhr01ByReqNo("SD22A0001");
+        System.out.print(tsdhr01.toString());
         //System.out.println(dog);
       //  System.out.println(person);
     }
