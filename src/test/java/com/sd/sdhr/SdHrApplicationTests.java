bc39a83a66d8aed0ee5752a756810edfbbee0f3a
bmgjdm <lkn301415@163.com>
2022-12-07 09:57:14 +0800

    lkn:初始换单元测试

--- src/test/java/com/sd/sdhr/SdHrApplicationTests.java
+++ src/test/java/com/sd/sdhr/SdHrApplicationTests.java
@@ -1,28 +1,35 @@
 package com.sd.sdhr;
 
 
-import com.sd.sdhr.pojo.Tsdhr01;
-import com.sd.sdhr.service.impl.Tsdhr01ServiceImpl;
+
+import com.sd.sdhr.service.common.EmailSendUtil;
+import com.sd.sdhr.service.sd.hr.impl.Tsdhr03ServiceImpl;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+
+import java.sql.SQLException;
+
 //单元测试
 @SpringBootTest
 class SdHrApplicationTests {
-    //@Autowired
-    //Dog dog;
-   //private Person person;
 
-    @Autowired
-    Tsdhr01ServiceImpl tsdhr01ServiceImpl;
+
 
     @Test
     void contextLoads() {
-        Tsdhr01 tsdhr01=tsdhr01ServiceImpl.queryTsdhr01ByReqNo("SD22A0001");
-        System.out.print(tsdhr01.toString());
+
+        System.out.print("");
         //System.out.println(dog);
       //  System.out.println(person);
     }
+
+
+    /*@Test
+    void contextLoadExper() throws Exception {
+        //emailSendUtil.sendSimpleHtmlMail("lkn301415@163.com","这是一个测试邮件下好大的哈");
+        emailSendUtil.resceiveMail("lkn301415@163.com","12313");
+    }*/
 
 }
