cf1f868ef98225c22de7ce33502498e677f33484
AMingZ1 <1513284947@qq.com>
2023-02-24 13:12:31 +0800

    提交

--- src/main/java/com/sd/sdhr/controller/sd/hr/Tsdhr04Controller.java
+++ src/main/java/com/sd/sdhr/controller/sd/hr/Tsdhr04Controller.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping(value = "/sdhr04")
+@RequestMapping(value = "/Sdhr04")
 @CrossOrigin
 public class Tsdhr04Controller {
 
