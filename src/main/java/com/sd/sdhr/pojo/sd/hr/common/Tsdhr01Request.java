7a86bcd8893fcb83756c0965f093ecd201629657
bmgjdm <lkn301415@163.com>
2023-02-28 16:51:43 +0800

    lkn:后台OFFER工作流流程调整xml

--- src/main/java/com/sd/sdhr/pojo/sd/hr/common/Tsdhr01Request.java
+++ src/main/java/com/sd/sdhr/pojo/sd/hr/common/Tsdhr01Request.java
@@ -1,13 +1,10 @@
 package com.sd.sdhr.pojo.sd.hr.common;
 
 import com.sd.sdhr.pojo.sd.hr.Tsdhr01;
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import lombok.*;
 
-@Data//瀹炵幇get+set+toString鍔熻兘
-@AllArgsConstructor//鏈夊弬鏋勯��
-@NoArgsConstructor//鏃犲弬鏋勯��
+@Getter
+@Setter
 public class Tsdhr01Request extends Tsdhr01 {
 
     private int  pageSize=10;
